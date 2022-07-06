653e443e0dd8546af0c3f89b616866c8912637fb
Dave Hart <dphart@rochester.rr.com>
2022-07-06 15:30:15 -0400

    Push event attachment

--- zhook.py
+++ zhook.py
@@ -14,9 +14,46 @@ channel = os.getenv("INPUT_DESTCHANNEL", "dev-notifications")
 actionRepo = os.getenv("GITHUB_ACTION_REPOSITORY", "ziti-mattermost-action")
 eventName = os.getenv("GITHUB_EVENT_NAME", "unspecified")
 
-def addFooter(attachment) :
-  attachment["footer"] = actionRepo
-  attachment["footer_icon"] = "https://github.com/openziti/branding/blob/main/images/ziggy/png/Ziggy-Gits-It.png?raw=true"
+def createTitle(eventJson):
+  repoJson = eventJson["repository"]
+  senderJson = eventJson["sender"]
+
+  title = f"{eventName.capitalize()}"
+
+  if eventName == "pull_request":
+    title = f"Pull request {eventJson['action']}"
+
+  return f"{title} by [{senderJson['login']}]({senderJson['html_url']}) in [{repoJson['name']}]({repoJson['html_url']})"
+
+def createAttachment(eventJson):
+  senderJson = eventJson["sender"]
+
+  attachment = {
+    "author_name": senderJson['login'],
+    "author_icon": senderJson['avatar_url'],
+    "author_link": senderJson['html_url'],
+    "footer": actionRepo,
+    "footer_icon": "https://github.com/openziti/branding/blob/main/images/ziggy/png/Ziggy-Gits-It.png?raw=true",
+  }
+
+  if eventName == "push":
+    commits = eventJson["commits"]
+    pushBody = f"[{len(commits)} commits]({eventJson['compare']}"
+    for c in commits:
+      pushBody = f"\n[`{c['id']}`](c['url']) {c['message']}"
+
+    attachment["text"] = pushBody
+  elif eventName == "pull_request":
+    prJson = eventJson["pull_request"]
+    attachment["color"] = "#00FF00"
+    attachment["title"] = prJson["title"]
+    attachment["title_link"] = prJson["html_url"]
+    attachment["text"] = f"{prJson['body'] or ''}\n#new-pull-request"
+    attachment["thumb_url"] = "https://github.com/openziti/branding/blob/main/images/ziggy/png/Ziggy-Gits-It.png?raw=true"
+  else:
+    print(f"Gotcha some other event {eventName}")
+
+  return attachment
 
 if __name__ == '__main__':
   # Temporaily print out the env... (TODO: remove this)
@@ -34,43 +71,17 @@ if __name__ == '__main__':
   #
   # Setup webhook JSON
   #
+  eventName = eventName.lower()
   eventJson = json.loads(eventJsonStr)
-  repoJson = eventJson["repository"]
-  senderJson = eventJson["sender"]
 
   body = {
     "username": username, 
     "icon_url": icon,
     "channel": channel,
+    "props": { "card": f"```json\n{eventJsonStr}\n```" },
   }
-  body["text"] = f"{eventName.capitalize()} by [{senderJson['login']}]({senderJson['html_url']}) in [{repoJson['name']}]({repoJson['html_url']})"
-
-  attachment = {
-    "color": "#00FF00",
-    "author_name": "smilindave26",
-    "author_icon": "https://avatars.githubusercontent.com/u/19175177?v=4",
-    "author_link": "https://github.com/smilindave26",
-    "title": "Update to TSDK v0.18.10 #137",
-    "title_link": "https://github.com/openziti/ziti-sdk-swift/pull/137",
-    "text": "No description provided.\n#new-pull-request",
-    "thumb_url": "https://github.com/openziti/branding/blob/main/images/ziggy/png/Ziggy-Gits-It.png?raw=true",
-  }
-  addFooter(attachment)
-
-  body["attachments"] = [attachment]
-
-  card = f"```json\n{eventJsonStr}\n```"
-  body["props"] = {
-    "card": card
-  }
-
-  eventName = eventName.lower()
-  if eventName == "push":
-    print("Gotcha PUSH")
-  elif eventName == "pull-request":
-    print("Gottha pull-request")
-  else:
-    print(f"Gotcha some other event {eventName}")
+  body["text"] = createTitle(eventJson)
+  body["attachments"] = [createAttachment(eventJson)]
 
   #
   # Post the webhook over Ziti
