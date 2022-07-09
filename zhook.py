0c3c8457975664fbf55b24ed08181127ab149039
Dave Hart <dphart@rochester.rr.com>
2022-07-08 20:15:10 -0400

    Re-organize a bit...

--- zhook.py
+++ zhook.py
@@ -1,148 +1,189 @@
 import requests
 import openziti
 import json
+import sys
 import os
 
-zitiId = os.getenv("INPUT_ZITIID")
-url = os.getenv("INPUT_WEBHOOKURL")
-eventJsonStr = os.getenv("INPUT_EVENTJSON")
+class MattermostWebhookBody:
+  actionRepoIcon = "https://github.com/openziti/branding/blob/main/images/ziggy/png/Ziggy-Gits-It.png?raw=true"
+  prThumbnail = "https://github.com/openziti/branding/blob/main/images/ziggy/png/Ziggy-Gits-It.png?raw=true"
+  prApprovedThumbnail = "https://raw.githubusercontent.com/openziti/branding/main/images/ziggy/closeups/Ziggy-Dabbing.png"
 
-username = os.getenv("INPUT_SENDERUSERNAME", "GitHubZ")
-icon = os.getenv("INPUT_SENDERICONULR", "https://github.com/fluidicon.png")
-channel = os.getenv("INPUT_DESTCHANNEL", "dev-notifications")
+  def __init__(self, username, icon, channel, eventName, eventJsonStr, actionRepo):
+    self.username = username
+    self.icon = icon
+    self.channel = channel
+    self.eventName = eventName.lower()
+    self.eventJsonStr = eventJsonStr
+    self.actionRepo = actionRepo
+    self.eventJson = json.loads(eventJsonStr)
+    self.repoJson = self.eventJson["repository"]
+    self.senderJson = self.eventJson["sender"]
 
-actionRepo = os.getenv("GITHUB_ACTION_REPOSITORY", "ziti-mattermost-action")
-eventName = os.getenv("GITHUB_EVENT_NAME", "unspecified")
+    self.body = {
+      "username": self.username, 
+      "icon_url": self.icon,
+      "channel": self.channel,
+      "props": { "card": f"```json\n{self.eventJsonStr}\n```" },
+    }
 
-def createTitle(eventJson):
-  repoJson = eventJson["repository"]
-  senderJson = eventJson["sender"]
+    self.attachment = {
+      "author_name": self.senderJson['login'],
+      "author_icon": self.senderJson['avatar_url'],
+      "author_link": self.senderJson['html_url'],
+      "footer": self.actionRepo,
+      "footer_icon": self.actionRepoIcon,
+    }
 
-  title = f"{eventName.capitalize().replace('_',' ')}"
+    if eventName == "push": self.addPushDetails()
+    elif eventName == "pull_request": self.addPullRequestDetails()
+    elif eventName == "pull_request_review_comment": self.addPullRequestReviewCommentDetails()
+    elif eventName == "pull_request_review": self.addPullRequestReviewDetails()
+    elif eventName == "delete": self.addDeleteDetails()
+    elif eventName == "create": self.addCreateDetails()
+    elif eventName == "issues": self.addIssuesDetails()
+    elif eventName == "issue_comment": self.addIssueCommentDetails()
+    elif eventName == "fork": self.addForkDetails()
+    elif eventName == "release": self.addReleaseDetails()
+    else: self.addDefaultDetails()
 
-  try:
-    action = eventJson["action"]
-    title += f" {action}"
-  except:
-    pass
+    self.body["attachments"] = [self.attachment]
 
-  return f"{title} by [{senderJson['login']}]({senderJson['html_url']}) in [{repoJson['full_name']}]({repoJson['html_url']})"  
+  def createTitle(self):
+    login = self.senderJson["login"]
+    loginUrl = self.senderJson["html_url"]
+    repoName = self.repoJson["full_name"]
+    repoUrl = self.repoJson["html_url"]
 
-def addPushDetails(eventJson, body, attachment):
-  body["text"] = createTitle(eventJson)
-  forced = eventJson["forced"]
-  commits = eventJson["commits"]
-  if forced:
-    pushBody = "Force-pushed "
-  else:
-    pushBody = "Pushed "
+    title = f"{self.eventName.capitalize().replace('_',' ')}"
 
-  pushBody += f"[{len(commits)} commit(s)]({eventJson['compare']}) to {eventJson['ref']}"
-  for c in commits:
-    pushBody += f"\n[`{c['id'][:6]}`]({c['url']}) {c['message']}"
-  attachment["color"] = "#000000"
-  attachment["text"] = pushBody
+    try:
+      action = self.eventJson["action"]
+      title += f" {action}"
+    except:
+      pass
 
+    return f"{title} by [{login}]({loginUrl}) in [{repoName}]({repoUrl})"  
 
-def createEventBody(eventName, eventJsonStr):
-  eventJson = json.loads(eventJsonStr)
-  repoJson = eventJson["repository"]
-  senderJson = eventJson["sender"]
+  def addPushDetails(self):
+    self.body["text"] = self.createTitle()
+    forced = self.eventJson["forced"]
+    commits = self.eventJson["commits"]
 
-  body = {
-    "username": username, 
-    "icon_url": icon,
-    "channel": channel,
-    "props": { "card": f"```json\n{eventJsonStr}\n```" },
-  }
+    if forced:
+      pushBody = "Force-pushed "
+    else:
+      pushBody = "Pushed "
 
-  attachment = {
-    "author_name": senderJson['login'],
-    "author_icon": senderJson['avatar_url'],
-    "author_link": senderJson['html_url'],
-    "footer": actionRepo,
-    "footer_icon": "https://github.com/openziti/branding/blob/main/images/ziggy/png/Ziggy-Gits-It.png?raw=true",
-  }
+    pushBody += f"[{len(commits)} commit(s)]({self.eventJson['compare']}) to {self.eventJson['ref']}"
+    for c in commits:
+      pushBody += f"\n[`{c['id'][:6]}`]({c['url']}) {c['message']}"
+    self.attachment["color"] = "#000000"
+    self.attachment["text"] = pushBody
 
-  if eventName == "push":
-    addPushDetails(eventJson, body, attachment)
-
-  elif eventName == "pull_request":
-    body["text"] = createTitle(eventJson)
-    prJson = eventJson["pull_request"]
-    attachment["color"] = "#00FF00"
-    attachment["title"] = prJson["title"]
-    attachment["title_link"] = prJson["html_url"]
+  def addPullRequestDetails(self):
+    self.body["text"] = self.createTitle()
+    prJson = self.eventJson["pull_request"]
+    self.attachment["color"] = "#00FF00"
+    self.attachment["title"] = prJson["title"]
+    self.attachment["title_link"] = prJson["html_url"]
 
     bodyTxt = prJson['body']
     if bodyTxt is not None:
       bodyTxt += "\n"
     bodyTxt += "#new-pull-request"
-    attachment["text"] = bodyTxt
+    self.attachment["text"] = bodyTxt
 
-    attachment["thumb_url"] = "https://github.com/openziti/branding/blob/main/images/ziggy/png/Ziggy-Gits-It.png?raw=true"
+    self.attachment["thumb_url"] = self.prThumbnail
   
-  elif eventName == "pull_request_review_comment":
-    body["text"] = createTitle(eventJson)
-    commentJson = eventJson["comment"]
-    prJson = eventJson['pull_request']
+  def addPullRequestReviewCommentDetails(self):
+    self.body["text"] = self.createTitle()
+    commentJson = self.eventJson["comment"]
+    prJson = self.eventJson['pull_request']
     bodyTxt = f"[Comment]({commentJson['html_url']}) in [PR#{prJson['number']}: {prJson['title']}]({prJson['html_url']}):\n"
     bodyTxt += f"{commentJson['body']}"
-    attachment["text"] = bodyTxt
-  
-  elif eventName == "pull_request_review":
-    body["text"] = createTitle(eventJson)
-    reviewJson = eventJson["review"]
+    self.attachment["text"] = bodyTxt
+
+  def addPullRequestReviewDetails(self):
+    self.body["text"] = self.createTitle()
+    reviewJson = self.eventJson["review"]
     reviewState = reviewJson['state']
-    prJson = eventJson['pull_request']
+    prJson = self.eventJson['pull_request']
     bodyTxt = f"[Review]({reviewJson['html_url']}) of [PR#{prJson['number']}: {prJson['title']}]({prJson['html_url']})\n"
     bodyTxt += f"Review State: {reviewState.capitalize()}\n"
     bodyTxt += f"{reviewJson['body']}"
-    attachment["text"] = bodyTxt
+    self.attachment["text"] = bodyTxt
 
     if reviewState == "approved":
-      attachment["color"] = "#00FF00"
-      attachment["thumb_url"] = "https://raw.githubusercontent.com/openziti/branding/main/images/ziggy/closeups/Ziggy-Dabbing.png"
+      self.attachment["color"] = "#00FF00"
+      self.attachment["thumb_url"] = self.prApprovedThumbnail
 
-  elif eventName == "delete":
-    body["text"] = createTitle(eventJson)
-    attachment["text"] = f"Deleted {eventJson['ref_type']} \"{eventJson['ref']}\""
+  def addDeleteDetails(self):
+    self.body["text"] = self.createTitle()
+    self.attachment["text"] = f"Deleted {self.eventJson['ref_type']} \"{self.eventJson['ref']}\""
 
-  elif eventName == "create":
-    body["text"] = createTitle(eventJson)
-    attachment["text"] = f"Created {eventJson['ref_type']} \"{eventJson['ref']}\""
-  
-  else:
-    attachment["color"] = "#FFFFFF"
-    attachment["text"] = createTitle(eventJson)
-    attachment["fallback"] = f"{eventName.capitalize().replace('_',' ')} by {senderJson['login']} in {repoJson['full_name']}"
+  def addCreateDetails(self):
+    self.body["text"] = self.createTitle()
+    self.attachment["text"] = f"Created {self.eventJson['ref_type']} \"{self.eventJson['ref']}\""
 
-  body["attachments"] = [attachment]
+  def addIssuesDetails(self):
+    self.body["text"] = self.createTitle()
+    self.attachment["text"] = "TODO"
 
-  return body
+  def addIssueCommentDetails(self):
+    self.body["text"] = self.createTitle()
+    self.attachment["text"] = "TODO"
+
+  def addForkDetails(self):
+    self.body["text"] = self.createTitle()
+    self.attachment["text"] = "TODO"
+
+  def addReleaseDetails(self):
+    self.body["text"] = self.createTitle()
+    self.attachment["text"] = "TODO"
+
+  def addDefaultDetails(self):
+    self.attachment["color"] = "#FFFFFF"
+    self.attachment["text"] = self.createTitle()
+    self.attachment["fallback"] = f"{eventName.capitalize().replace('_',' ')} by {self.senderJson['login']} in {self.repoJson['full_name']}"
+
+  def dumpJson(self): 
+    return json.dumps(self.body)
 
 if __name__ == '__main__':
+  zitiId       = os.getenv("INPUT_ZITIID")
+  url          = os.getenv("INPUT_WEBHOOKURL")
+  eventJsonStr = os.getenv("INPUT_EVENTJSON")
+  username     = os.getenv("INPUT_SENDERUSERNAME")
+  icon         = os.getenv("INPUT_SENDERICONULR")
+  channel      = os.getenv("INPUT_DESTCHANNEL")
+  actionRepo   = os.getenv("GITHUB_ACTION_REPOSITORY")
+  eventName    = os.getenv("GITHUB_EVENT_NAME")
+
   # Setup Ziti identity
   idFilename = "id.json"
   os.environ["ZITI_IDENTITIES"] = idFilename
   with open(idFilename, 'w') as f:
     f.write(zitiId)
 
-  # Setup webhook JSON
-  eventName = eventName.lower()
-  body = createEventBody(eventName, eventJsonStr)
+  # Create webhook body
+  try:
+    mwb = MattermostWebhookBody(username, icon, channel, eventName, eventJsonStr, actionRepo)
+  except Exception as e:
+    print(f"Exception creating webhook body: {e}")
+    sys.exit(-1)
 
   # Post the webhook over Ziti
   headers = {'Content-Type': 'application/json',}
-  jsonData = json.dumps(body)
-  print(f"{jsonData}")
+  data = mwb.dumpJson()
+  print(f"{data}")
 
   with openziti.monkeypatch():
     try:
-      r = requests.post(url, headers=headers, data=jsonData)
+      r = requests.post(url, headers=headers, data=data)
       print(f"Response Status: {r.status_code}")
       print(r.headers)
       print(r.content)
     except Exception as e:
-      print(f"Error posting webhook: {e}")
-
+      print(f"Exception posting webhook: {e}")
+      sys.exit(-1)
